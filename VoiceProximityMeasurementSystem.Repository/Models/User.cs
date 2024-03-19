8e9b91ea8c5766b249aa20db29f5307b110c9456
NguyenChienThang <chienthang014@gmail.com>
2024-03-19 14:57:57 +0700

    Add model and nuget

---
+++ VoiceProximityMeasurementSystem.Repository/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VoiceProximityMeasurementSystem.Repository.Models;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public double? Result { get; set; }
+
+    public string Name { get; set; }
+
+    public virtual ICollection<UserAnswer> UserAnswers { get; set; } = new List<UserAnswer>();
+}
---
+++ VoiceProximityMeasurementSystem.Repository/Models/UserAnswer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VoiceProximityMeasurementSystem.Repository.Models;
+
+public partial class UserAnswer
+{
+    public int UserAnswerId { get; set; }
+
+    public int? LandoltId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public virtual LandoltE Landolt { get; set; }
+
+    public virtual User User { get; set; }
+}
